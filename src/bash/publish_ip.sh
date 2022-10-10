3ae1f223d3952048236be8ff39e53237fc3ffe24
Heiko Kromer <h@heikokromer.com>
2022-10-10 19:42:38 +0200

    replaced username

--- src/bash/publish_ip.sh
+++ src/bash/publish_ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 2 &&
-/home/wilson/env/bin/python /home/wilson/company-conference-iot/src/python/publish_ip.py
+/home/doneX/env/bin/python /home/doneX/company-conference-iot/src/python/publish_ip.py
