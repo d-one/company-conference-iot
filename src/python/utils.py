011c290e3a85fffca03b286fdb0867b4ada07f14
kromerh <HeikoKromer@user-goldens-MacBook-Pro.local>
2022-10-26 12:30:38 +0200

    added docstring, updated typings

--- src/python/utils.py
+++ src/python/utils.py
@@ -4,6 +4,7 @@ Author:      Heiko Kromer - D ONE - 2022
 Description: This script contains utility functions used in company-conference-iot
 ==================================================================================
 """
+from io import TextIOWrapper
 from typing import Tuple, Optional
 from datetime import datetime
 import logging
@@ -16,10 +17,18 @@ logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
 
-def safe_open_folder(path, mode):
-    ''' Open "path" for writing, creating any parent directories as needed.
-    '''
+def safe_open_folder(path: str, mode: str) -> TextIOWrapper:
+    """Function to open "path" for writing, creating any parent directories as needed.
+
+    Args:
+        path: Path to open for creating any parent directories if needed.
+        mode: OpenTextMode as per definition in python built-in function `open()`.
+
+    Returns:
+        TextIOWrapper object to use in place of `open()`.
+    """
     os.makedirs(os.path.dirname(path), exist_ok=True)
+    
     return open(path, mode)
 
 def log(log_path: str, logmsg: str, printout: bool = False) -> None:
@@ -39,7 +48,7 @@ def log(log_path: str, logmsg: str, printout: bool = False) -> None:
     if printout:
         logger.info(f"## Added log message: {logmsg}.")
 
-    with safe_open_folder(log_path, 'a+') as file:
+    with safe_open_folder(path=log_path, mode='a+') as file:
         file.write('\n')
         file.write(logmsg)
         file.close()
